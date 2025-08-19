13a615a452270bb1c7b1cb2daa8ae8d98e4b68bb
Ragul Anastraj M <ragulanastrajm@gmail.com>
2025-08-19 23:17:49 +0530

    Add files via upload

---
+++ src/data/constants.js
@@ -0,0 +1,297 @@
+export const Bio = {
+  name: "Ragul Anastraj",
+  roles: [
+    "Full Stack Developer",
+   
+  ],
+  description:
+    "I am a Full Stack Developer with a passion for building web applications and learning new technologies. I have experience in both frontend and backend development, and I am always looking to improve my skills and knowledge.",
+  github: "http://github.com/RagulAnastrajM",
+  resume:
+    "https://github.com/RagulAnastrajM/resume/blob/main/Ragul%20Anastraj%20Resume.pdf",
+  linkedin: "https://www.linkedin.com/in/ragulanastraj7",
+  twitter: "https://x.com/ragul83246?t=0GgJP2El2t6zgT60tqLOkQ&s=08",
+  insta: "https://www.instagram.com/ragul_x14/",
+  facebook: "https://www.facebook.com/share/1NrQ2hQqUQ/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      },
+    
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      
+     
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+     
+     
+     
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+    ],
+  },
+  
+  {
+    title: "Others",
+    skills: [
+    
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+      
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+     
+     
+    ],
+  },
+];
+
+export const experiences = [
+  
+
+  {
+    id: 4,
+    img: "http://sdprosolutions.com/wp-content/uploads/2018/12/sd-pro-logo.png",
+    role: "Cloud Computing Intern",
+    company: "sd pro solutions",
+    date: "July 2024 - August 2024",
+    desc: "During my internship at SD Pro Solutions, I worked on cloud computing projects, focusing on deploying and managing applications in cloud environments. I gained hands-on experience with AWS services, containerization using Docker, and CI/CD pipelines. This experience enhanced my understanding of cloud architecture and best practices for scalable application deployment.",
+    skills: [
+      "Cloud service and deployment models",
+      "Virtualization and storage solutions",
+      "Cloud security fundamentals",
+      "Basics of AWS/Azure services ",
+      
+    ],
+    
+    doc: "https://raw.githubusercontent.com/RagulAnastrajM/internship-certificate/52a9c440d894b7e70e3997a09827026402873842/certificate.jpg",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://th.bing.com/th/id/ODF.mSG_Mqvrnfk_Y2-3bBhfVA?w=32&h=32&qlt=90&pcl=fffffa&o=6&pid=1.2",
+    school: "Arasu Engineering College",
+    date: "Oct 2021 - Sep 2025",
+    grade: "8.21 CGPA",
+    desc: " I am currently pursuing a Bachelor of Engineering (B.E.) in Computer Science and Engineering at Arasu Engineering College, with a strong academic record and a CGPA of 8.21",
+    degree: "Bachelor of Engineering- BE, Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    img: "https://tse1.mm.bing.net/th/id/OIP.9H4o0S2VPFjl15udLfk9cwHaGf?pid=ImgDet&w=160&h=140&c=7&dpr=1.3&o=7&rm=3",
+    school: "Town Higher Secondary School,Kumbakonam",
+    date: "Apr 2019 - Apr 2021",
+    grade: "84%",
+    desc: "I completed my Higher Secondary Certificate (HSC) with a focus on Bio-Mathematics at Town Higher Secondary School, Kumbakonam. This phase helped me build a strong foundation in both biological sciences and mathematics, fostering analytical thinking and academic discipline.",
+    degree: "HSC(XII), Bio Maths ",
+  },
+  {
+    id: 2,
+    img: "https://th.bing.com/th/id/ODF.qwB2wWSO8WcoY_rHHJw6-Q?w=32&h=32&qlt=90&pcl=fffffa&o=6&pid=1.2",
+    school: "Government Higher secondary School ,Udaiyalur",
+    date: "Apr 2018 - Apr 2019",
+    grade: "80%",
+    desc: "I completed my Secondary School Leaving Certificate (SSLC) at Government Model Higher Secondary School, Udaiyalur. This stage laid the academic groundwork and helped me develop discipline, curiosity, and strong foundational knowledge across subjects.",
+    degree: "SSLC(X)",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Theme-Calculator  ",
+    date: "Feb 2025 - Feb  2025",
+    description:
+      "Developed a stylish and interactive calculator with dynamic theme-switching functionality. This web-based tool allows users to perform basic arithmetic operations with a sleek, responsive user interface and real-time theme toggling for dark/light modes.",
+    image: "https://github.com/RagulAnastrajM/Theme-Calculator/blob/69018acb9d077bc1c77beab40b7dcae6724f5da0/calculator.png.png?raw=true",
+
+    tags: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+     
+    ],
+    category: "web app",
+    github: "https://github.com/RagulAnastrajM/Theme-Calculator",
+    webapp: " https://calculator-49571.web.app",
+  },
+  {
+    id: 0,
+    title: "Todo-List ",
+    date: "March 2025 - March 2025",
+    description:
+      "Developed a dynamic and user-friendly To-Do List application using React.js. The app enables users to add, view, and delete tasks efficiently, providing a responsive and intuitive interface for daily task management.",
+    image:
+      "https://raw.githubusercontent.com/RagulAnastrajM/Todo-List/716c8393e3eb785de8c149b55cb8e7e05bf13c29/todo-app.png",
+    tags: ["React Js"],
+    category: "web app",
+    github: "https://github.com/RagulAnastrajM/Todo-List",
+    webapp: "https://todo-app-870c7.web.app",
+  
+  },
+  {
+    id: 1,
+    title: "Employee-Management-System ",
+    date: "Mar 2025 - Mar 2025",
+    description:
+      "Built a full-featured Employee Management System using React.js that allows users to add, edit, and delete employee records. The system uses JSON data structures to manage and display employee information dynamically within a responsive interface.",
+    image:
+      "https://raw.githubusercontent.com/RagulAnastrajM/Employee-Management-System/b9f90c86b920f92493d8d8ff5cae82c6e7aed522/EMS.png",
+    tags: [
+      "React Js",
+      "json",
+      "Redux",
+      
+    ],
+    category: "web app",
+    github: "https://github.com/RagulAnastrajM/Employee-Management-System",
+    webapp: "https://ems-181d5.web.app",
+  },
+  
+  
+ 
+  {
+    id: 4,
+    title: "Mens-store-website",
+    date: "April 2025 - April 2025",
+    description:
+      " Developed a responsive and user-friendly online men's fashion store using React.js and Redux for state management. The project includes dynamic product listings, filtering, and a fully functional Add to Cart system",
+    image:
+      "https://raw.githubusercontent.com/RagulAnastrajM/Men-s-store-website/357821555cc7a3129066323a1a03f15d72e4c3d6/mens%20store1.png",
+    tags: ["React Js", "Redux", "CSS", "javaScript"],
+    category: "web app",
+    github: "https://github.com/RagulAnastrajM/Men-s-store-website",
+    webapp: "https://mens-store-f8c0f.web.app",
+  },
+  {
+    id: 5,
+    title: "Gpay-Login-Page ",
+    date: "May 2021",
+    description:
+      " Developed a responsive Google Pay (GPay) login page interface using React.js, simulating the modern UI/UX design of digital payment platforms. The project showcases form validation, user interaction handling, and component-based structure, ideal for demonstrating front-end authentication flows.",
+    image:
+      "https://raw.githubusercontent.com/RagulAnastrajM/Gpay-Login-Page/b2fb73d1af0a87bbfc79ef290e29227a2d6815a8/gpay.png",
+    tags: ["React Js", "CSS", "javascript",],
+    category: "web app",
+    github: "https://github.com/RagulAnastrajM/Gpay-Login-Page",
+    webapp: "https://gpay-login.web.app",
+  },
+  {
+    id: 6,
+    title: "BMI-Calculator ",
+    date: "Jun 2025",
+    description:
+      "Designed and developed a Body Mass Index (BMI) Calculator using pure HTML, CSS, and JavaScript. This project calculates the user's BMI based on height and weight inputs, and categorizes the result according to standard health ranges.",
+    image:
+      "https://raw.githubusercontent.com/RagulAnastrajM/BMI-Calculator/8115aca3029578457d0b264e54e4a7b447a93844/BMI.png",
+    tags: ["HTML", "CSS", "Javascript", ],
+    category: "web app",
+    github: "https://github.com/RagulAnastrajM/BMI-Calculator",
+    webapp: "https://bmi-calculator-59eda.web.app",
+  },
+   {
+    id: 7,
+    title: "Victus Store",
+    date: "Jun 2025",
+    description:
+      "Developed a user-friendly front-end eCommerce website named Victus Store, focused on delivering a smooth and responsive shopping experience. Built using modern web technologies to replicate an online store layout with product listings, cart UI, and navigation features.",
+    image:
+      "https://raw.githubusercontent.com/RagulAnastrajM/Victus-store-Ecommerce-website-/d80ef82cf60bb0060e40b34e83e093d96cc5974a/VICTUS.png",
+    tags: ["HTML", "CSS", "Javascript", ],
+    category: "web app",
+    github: "https://github.com/RagulAnastrajM/Victus-store-Ecommerce-website-",
+    webapp: "https://victus-store-d82d9.web.app",
+  },
+   {
+    id: 8,
+    title: "Age Calculator",
+    date: "Jun 2025",
+    description:
+      "Created a dynamic and responsive Age Calculator web app that allows users to input their birthdate and instantly calculates their exact age in years, months, and days. Designed with a user-friendly interface and real-time output using core web technologies.",
+    image:
+      "https://raw.githubusercontent.com/RagulAnastrajM/Age-Calculator/7de8ded9a6124e8dcb5f53b58f4adace306efb9c/Age.png",
+    tags: ["HTML", "CSS", "Javascript", ],
+    category: "web app",
+    github: "https://github.com/RagulAnastrajM/Age-Calculator",
+    webapp: "https://age-calculator-d7ba5.web.app",
+  },
+   {
+    id: 9,
+    title: "Flipkart Clone",
+    date: "Jun 2025",
+    description:
+      "Developed a front-end clone of the popular eCommerce platform Flipkart, replicating its core design structure and UI elements. The project focuses on layout precision, responsive design, and interactive features using only front-end technologies.",
+    image:
+      "https://raw.githubusercontent.com/RagulAnastrajM/Flipkart-Login-Page/1832c9593c1c1ced1190ba49db5f9607723ad58b/flip.png",
+    tags: ["HTML", "CSS", "Javascript", ],
+    category: "web app",
+    github: "https://github.com/RagulAnastrajM/Flipkart-Login-Page",
+    webapp: "https://flipkart-clone-page.web.app",
+  },
+  
+  
+];
+
